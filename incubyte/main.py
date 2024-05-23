d259cbfc0f0ad5edb9bca381f539ea383ffbba44
ajay solanke <ajaysolanke1531991@gmail.com>
2024-05-23 19:04:42 +0530

    added support for custom delimiter; throwing exception on negative number

--- incubyte/main.py
+++ incubyte/main.py
@@ -1,12 +1,20 @@
 def add(s):
     if s=="":
         return 0
-    nums = [ int(x.strip()) for x in s.split(',')]
+    delim = ","
+    if s.startswith("//"):
+        delim = s[2]
+        s = s[3:]
+
+    nums = [ int(x.strip()) for x in s.split(delim)]
     sum = 0
+    
     for n in nums:
+        if n<0:
+            raise Exception(f"negative numbers not allowed {str(n)}")
         sum += n
     return sum
 
 
 if __name__ == '__main__':
-    add("")
+    print(add("//;\n1;2;-3"))
--- incubyte/test.py
+++ incubyte/test.py
@@ -6,3 +6,8 @@ assert add("1") == 1
 assert add("1,5") == 6
 assert add("1,2,3,4,5") == 15
 assert add("1\n,2,3") == 6
+assert add("//;\n1;2;3") == 6
+
+with pytest.raises(Exception) as e_info:
+    add("1,-2,3")
+    assert str(e_info.value) == 'negative numbers not allowed -2'

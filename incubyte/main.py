e255a2e7208e4707facb0c0156e51ecca128a63f
ajay solanke <ajaysolanke1531991@gmail.com>
2024-05-23 18:48:59 +0530

    newlines allowed in betwwen numbers

--- incubyte/main.py
+++ incubyte/main.py
@@ -1,7 +1,7 @@
 def add(s):
     if s=="":
         return 0
-    nums = [ int(x) for x in s.split(',')]
+    nums = [ int(x.strip()) for x in s.split(',')]
     sum = 0
     for n in nums:
         sum += n
--- incubyte/test.py
+++ incubyte/test.py
@@ -4,3 +4,5 @@ from main import add
 assert add("") == 0
 assert add("1") == 1
 assert add("1,5") == 6
+assert add("1,2,3,4,5") == 15
+assert add("1\n,2,3") == 6
